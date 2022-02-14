93fa24a6ba4d8d62de0af1cf2cb468549deed29c
RupendraSinghBhadoriya <rupendra.bhadoriya03@gmail.com>
2022-02-15 01:24:45 +0530

    add dotenv fix

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('./dotenv').config();
 
 const JWTSECRET = process.env.JWTSECRET;
 // const DB_USERNAME = process.env.DB_USERNAME;
@@ -6,9 +6,7 @@ const DB_USERNAME = 'rupendra';
 const DB_PASSWORD = 'IkkH7v2q59nNQha6';
 // const DB_PASSWORD = process.env.DB_PASSWORD;
 // const DBURL = `mongodb+srv://<rupendra>:<password>@dgtlz-finance.a7tbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-module.exports = {
-    jwtSecret: JWTSECRET,
-    mongodburi: 'mongodb+srv://' + DB_USERNAME + ':' + DB_PASSWORD + '@dgtlz-finance.a7tbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-};
+export const jwtSecret = JWTSECRET;
+export const mongodburi = 'mongodb+srv://' + DB_USERNAME + ':' + DB_PASSWORD + '@dgtlz-finance.a7tbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 // mongodb+srv://rupendra:<password>@dgtlz-node-app-with-rea.a7tbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
